e6a64c1516b55447f86b02fb2fc2c1763440bdf9
J. King <jking@jkingweb.ca>
2015-09-23 22:58:35 -0400

    Basic working TOTP implementation
    Implemented gen_time() function to pass as counter.  Counter in
    gen_otp() may now also be int or float.

--- lib.otp.php
+++ lib.otp.php
@@ -5,7 +5,8 @@ function gen_otp($secret, $counter, $digits = 6, $mode = "sha1") {
 	// number of digits must be either 6, 7, or 8)
 	$digits = min(max($digits,6),8);
 	// message to be hashed must be an 8-byte string
-	$C = str_pad($counter, 8, chr(0), STR_PAD_LEFT);
+	if(is_int($counter) || is_double($counter)) $counter = hex2bin(str_pad(dechex(floor($counter)),16,"0", STR_PAD_LEFT));
+	$counter = str_pad($counter, 8, chr(0), STR_PAD_LEFT);
 	// hash the counter with the key
 	$hash = hash_hmac($mode, $counter, $secret, false);
 	// get the truncation offset)
@@ -20,3 +21,13 @@ function gen_otp($secret, $counter, $digits = 6, $mode = "sha1") {
 	return $otp;
 }
 
+function gen_time($step = 30, $epoch = 0, $time = NULL) {
+	// step must be at least one second
+	$step = (int) max($step, 1);
+	// specification says nothing about negative epochs, so I presume they are allowed
+	$epoch = (int) $epoch;
+	// assume current time if not supplied
+	if($time===NULL) $time = time();
+	$time = (int) $time;
+	return (int) floor(($time - $epoch) / $step);
+}
