f9ff6506251e450ef67c0aa91f435a9cebd7384b
J. King <jking@jkingweb.ca>
2015-09-27 17:01:59 -0400

    Changed function name arrangement; added license

--- lib.otp.php
+++ lib.otp.php
@@ -1,14 +1,47 @@
 <?php
+/*
+   Genuflect one-time password library for PHP
+    by J. King (http://jkingweb.ca/)
+   Licensed under MIT license
+
+   Last revised 2015-09-27
+*/
+
+/*
+Copyright (c) 2015 J. King
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 namespace Genuflect;
 
-function gen_otp($secret, $counter, $digits = 6, $mode = "sha1") {
+function otp_gen($secret, $counter, $digits = 6, $mode = "sha1") {
 	// number of digits must be either 6, 7, or 8)
 	$digits = min(max($digits,6),8);
 	// message to be hashed must be an 8-byte string
 	if(is_int($counter) || is_double($counter)) $counter = hex2bin(str_pad(dechex(floor($counter)),16,"0", STR_PAD_LEFT));
 	$counter = str_pad($counter, 8, chr(0), STR_PAD_LEFT);
 	// hash the counter with the key
-	$hash = hash_hmac($mode, $counter, $secret, false);
+	$hash = hash_hmac($mode, $counter, $secret);
 	// get the truncation offset)
 	$offset = hexdec(substr($hash, -1, 1));
 	// truncate the hash to four bytes
@@ -21,7 +54,7 @@ function gen_otp($secret, $counter, $digits = 6, $mode = "sha1") {
 	return $otp;
 }
 
-function gen_time($step = 30, $epoch = 0, $time = NULL) {
+function otp_gen_time($step = 30, $epoch = 0, $time = NULL) {
 	// step must be at least one second
 	$step = (int) max($step, 1);
 	// specification says nothing about negative epochs, so I presume they are allowed
@@ -31,3 +64,4 @@ function gen_time($step = 30, $epoch = 0, $time = NULL) {
 	$time = (int) $time;
 	return (int) floor(($time - $epoch) / $step);
 }
+
